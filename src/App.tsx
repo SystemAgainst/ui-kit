ad66801884c4ebf292c28202220c4da5fa7c4a7a
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-11-07 13:12:34 +1000

    add: typography component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import './App.css';
-import Accordion from "./components/Accordion/Accordion.tsx";
-import Table from "./components/Table";
-import MockData from "./components/Table/data/mock.data.ts";
-import { accordionItems } from "./components/Accordion/mock.data.tsx";
-import styles from "./components/Table/style/style.module.css";
-
+import './App.css'
+import Accordion from './components/Accordion/Accordion.tsx'
+import Typography from './components/Typography'
+// import Table from './components/Table'
+// import MockData from './components/Table/data/mock.data.ts'
+import { accordionItems } from './components/Accordion/mock.data.tsx'
+// import styles from './components/Table/style/style.module.css'
 
 function App() {
-  return (
-    <div className="container">
-        <Accordion items={accordionItems} />
-        <div></div>
-        <div className={styles["table-container"]}>
-            <Table labels={MockData.labels} orderObj={MockData.orderObj} values={MockData.values} />
-        </div>
-    </div>
-  )
+	return (
+		<div className='container'>
+			<Accordion items={accordionItems} />
+			<Typography variant={'h3'}>asdf</Typography>
+			{/* <div className={styles['table-container']}>
+				<Table labels={MockData.labels} orderObj={MockData.orderObj} values={MockData.values} />
+			</div> */}
+		</div>
+	)
 }
 
 export default App
